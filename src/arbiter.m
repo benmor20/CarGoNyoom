066b9123118fc8760685e3bed213f9c526af6367
benmor20 <benmor2020@gmail.com>
2022-04-29 19:24:39 -0400

    MVP draft

--- src/arbiter.m
+++ src/arbiter.m
@@ -5,22 +5,29 @@ classdef arbiter
 
     properties
         brainwave_vec
-        result_vec
+        vel_result_vec
+        ang_result_vec
     end
 
     methods
-        function obj = arbiter(brainwave_vec, result_vec)
+        function obj = arbiter(brainwave_vec, vel_result_vec, ang_result_vec)
             obj.brainwave_vec = brainwave_vec;
-            obj.result_vec = result_vec;
+            obj.vel_result_vec = vel_result_vec;
+            obj.ang_result_vec = ang_result_vec;
         end
 
-        function result = arbitrate(obj)
-            total_wave = zeros(size(obj.brainwave_vec));
+        function [vel_result, ang_result] = arbitrate(obj)
+            total_vel = zeros(size(obj.brainwave_vec));
+            total_ang = zeros(size(obj.brainwave_vec));
             for wave_func = obj.brainwave_vec
-                total_wave = total_wave + wave_func();
+                [vel, ang] = wave_func();
+                total_vel = total_vel + vel;
+                total_ang = total_ang + ang;
             end
-            [~, I] = max(total_wave);
-            result = obj.result_vec(I);
+            [~, Ivel] = max(total_vel);
+            [~, Iang] = max(total_ang);
+            vel_result = obj.vel_result_vec(Ivel);
+            ang_result = obj.ang_result_vec(Iang);
         end
     end 
 end 
